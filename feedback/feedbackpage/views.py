8a7ee933daeb48e0a010af359e5a8af8e1740ce8
Shoham <shhmchk@gmail.com>
2019-09-24 13:52:24 +0800

    changed overview page and added new fields

--- feedback/feedbackpage/views.py
+++ feedback/feedbackpage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import fbcontent
-import feedbackpage.cloud
+#import feedbackpage.cloud
 
 # Create your views here.
 
@@ -10,10 +10,10 @@ def mainPage (request):
 
 def addfb (request):
     #create new item and save
-    newitem = fbcontent(stdname=request.POST.get('stdname'), feedback=request.POST.get('message'))
+    newitem = fbcontent(name=request.POST.get('stdname'), feedback=request.POST.get('message'), stdid=request.POST.get('stdid'), email=request.POST.get('stmail'), pillar=request.POST.get('pillar'), category=request.POST.get('category'))
     newitem.save()
 
     #cloud.main()
-    feedbackpage.cloud.main()
+    #feedbackpage.cloud.main()
 
     return HttpResponseRedirect ('/main/')
